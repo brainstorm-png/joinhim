87c881272659b3e28922bdcf35d5743376565f9c
brainstorm-png <fabifabregas85@gmail.com>
2026-01-21 22:37:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function checkPassword() {
-  const input = document.getElementById("password").value.toLowerCase();
+  const input = document
+    .getElementById("password")
+    .value
+    .toLowerCase()
+    .trim();
+
   const message = document.getElementById("message");
 
-  if (input === "frank sinatra") or (input === "sinatra") {
+  if (input === "frank sinatra" || input === "sinatra") {
     window.location.href = "wrong.html";
   } 
   else if (input === "born free") {
     window.location.href = "stage2.html";
+  } 
+  else if (input === "vexxed") {
+    window.location.href = "vexxed.html";
   } 
   else {
     message.textContent = "ACCESS DENIED";
